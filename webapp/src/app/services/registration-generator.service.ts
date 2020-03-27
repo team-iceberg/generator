a542b9d23445848f3f18a2d2e4c39da97a00a57f
Camille LEVOYE <camille.levoye@ext.leroymerlin.fr>
2020-03-27 16:23:06 +0100

    feat(import): add error message

--- webapp/src/app/services/registration-generator.service.ts
+++ webapp/src/app/services/registration-generator.service.ts
@@ -1,15 +1,21 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Injectable} from '@angular/core';
+import {ToastService} from '../shared/toast/toast.service';
 
 @Injectable()
 export class RegistrationGeneratorService {
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private toastService: ToastService) {
     }
 
     generateRegistration(file: File) {
         const formData = new FormData();
         formData.append('file', file, file.name);
-        this.http.post<void>('generator/membership/file', formData).subscribe();
+        this.http.post<void>('generator/membership/file', formData).subscribe(value => {
+                this.toastService.info('La génération des fiches en cours');
+            },
+            (err: HttpErrorResponse) => {
+                this.toastService.alert('Une erreur est survenue lors de la génération des fiches');
+            });
     }
 }
